ac905569a97154f6b64e7f6409fde076e01a45d2
Vikas Singh <Vikas@olibr.com>
2024-02-03 17:26:39 +0530

    bg-handle custom

--- 01background_changer/src/App.jsx
+++ 01background_changer/src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 function App() {
   const [color, setColor] = useState("grey");
 
+function redUpdate(){
+  setColor("red")
+}
+
   return (
     <>
       <div
@@ -12,9 +16,13 @@ function App() {
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-4 shadow-xl bg-white px-3 py-2 rounded-3xl ">
             <button
-              onClick={() => {
-                setColor("red");
-              }}
+              onClick={redUpdate
+                
+              //   () => {
+              //   setColor("red");
+              // }
+            
+            }
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-red-700"
             >
               Red
